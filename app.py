0b259471e7d7b66ed3bb870e85b3c334388e043c
gj-entertain <gj_entertain@outlook.com>
2023-05-04 11:07:10 -0400

    For Five more functions implementation: Users and user keys & User profiles (needs user) & Threaded replies & Date- and time-based range queries & User-based range queries (needs user)

--- app.py
+++ app.py
@@ -7,154 +7,174 @@ app = Flask(__name__)
 posts = []
 users = []
 
+
 def generate_key(length=12):
-    """Generate a unique key."""
     chars = string.ascii_letters + string.digits
     return ''.join(random.choices(chars, k=length))
 
-def get_post_by_id(id):
-    """Return the post with the given ID, or None if not found."""
+
+def find_post_by_id(post_id):
     for post in posts:
-        if post['id'] == id:
+        if post['id'] == post_id:
             return post
     return None
 
-def get_user_by_id(id):
-    """Return the user with the given ID, or None if not found."""
+
+def find_user_by_id(user_id):
     for user in users:
-        if user['id'] == id:
+        if user['id'] == user_id:
             return user
     return None
 
-def get_user_by_unique_key(unique_key):
-    """Return the user with the given unique_key, or None if not found."""
-    for user in users:
-        if user['unique_key'] == unique_key:
-            return user
-    return None
+
+def create_user(username, real_name=None, avatar=None):
+    user_key = generate_key()
+    user_id = len(users) + 1
+    user = {'id': user_id, 'username': username, 'key': user_key, 'real_name': real_name, 'avatar': avatar}
+    users.append(user)
+    return user
+
 
 @app.route('/user', methods=['POST'])
-def create_user():
-    """Create a new user with a unique ID, key, and unique_key."""
+def create_user_route():
     if not request.is_json:
         return jsonify({'err': 'Request body must be JSON'}), 400
 
-    unique_key = request.json.get('unique_key')
-    if not unique_key or not isinstance(unique_key, str) or get_user_by_unique_key(unique_key):
-        return jsonify({'err': 'Request body must contain a unique "unique_key" field with a string value'}), 400
+    username = request.json.get('username')
+    if not username or not isinstance(username, str):
+        return jsonify({'err': 'Request body must contain a "username" field with a string value'}), 400
 
-    non_unique_key = request.json.get('non_unique_key', '')
+    user = create_user(username)
+    return jsonify({'id': user['id'], 'username': user['username'], 'key': user['key']}), 201
 
-    id = len(users) + 1
-    while get_user_by_id(id):
-        id += 1
 
-    key = generate_key()
-    user = {'id': id, 'key': key, 'unique_key': unique_key, 'non_unique_key': non_unique_key}
-    users.append(user)
-
-    return jsonify({'id': id, 'key': key, 'unique_key': unique_key, 'non_unique_key': non_unique_key}), 201
-
-@app.route('/user/<int:id>', methods=['GET'])
-def get_user(id):
-    """Retrieve the user with the given ID."""
-    user = get_user_by_id(id)
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = find_user_by_id(user_id)
     if not user:
         return jsonify({'err': 'User not found'}), 404
 
-    return jsonify({'id': user['id'], 'unique_key': user['unique_key'], 'non_unique_key': user['non_unique_key']}), 200
+    return jsonify({'id': user['id'], 'username': user['username'], 'real_name': user['real_name'], 'avatar': user['avatar']}), 200
 
-@app.route('/user/<int:id>/edit', methods=['POST'])
-def edit_user(id):
-    """Edit the user with the given ID."""
-    user = get_user_by_id(id)
-    if not user:
-        return jsonify({'err': 'User not found'}), 404
 
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
     if not request.is_json:
         return jsonify({'err': 'Request body must be JSON'}), 400
+
+    user = find_user_by_id(user_id)
+    if not user:
+        return jsonify({'err': 'User not found'}), 404
 
     user_key = request.json.get('key')
-    if not user_key or user_key != user['key']:
+    if user['key'] != user_key:
         return jsonify({'err': 'Forbidden'}), 403
 
-    non_unique_key = request.json.get('non_unique_key')
-    if non_unique_key:
-        user['non_unique_key'] = non_unique_key
+    real_name = request.json.get('real_name')
+    avatar = request.json.get('avatar')
 
-    return jsonify({'id': user['id'], 'unique_key': user['unique_key'], 'non_unique_key': user['non_unique_key']}), 200
+    if real_name:
+        user['real_name'] = real_name
+    if avatar:
+        user['avatar'] = avatar
+
+    return jsonify({'id': user['id'], 'username': user['username'], 'real_name': user['real_name'], 'avatar': user['avatar']}), 200
+
 
 @app.route('/post', methods=['POST'])
 def create_post():
-    """Create a new post with a unique ID, key, and timestamp."""
     if not request.is_json:
         return jsonify({'err': 'Request body must be JSON'}), 400
 
-    msg = request.json.get('msg')
-    if not msg or not isinstance(msg, str):
+    message = request.json.get('msg')
+    if not message or not isinstance(message, str):
         return jsonify({'err': 'Request body must contain a "msg" field with a string value'}), 400
 
     user_id = request.json.get('user_id')
     user_key = request.json.get('user_key')
-    user = None
-    if user_id:
-        user = get_user_by_id(user_id)
+    if user_id and user_key:
+        user = find_user_by_id(user_id)
         if not user or user['key'] != user_key:
-            return jsonify({'err': 'Invalid user credentials'}), 403
+            return jsonify({'err': 'Invalid user ID or key'}), 400
 
-    id = len(posts) + 1
-    while get_post_by_id(id):
-        id += 1
+        parent_id = request.json.get('parent_id')
+        if parent_id:
+            parent_post = find_post_by_id(parent_id)
+            if not parent_post:
+                return jsonify({'err': 'Parent post not found'}), 400
 
-    key = generate_key()
-    timestamp = datetime.utcnow().isoformat()
-    reply_to = request.json.get('reply_to', None)
+        post_id = len(posts) + 1
+        while find_post_by_id(post_id):
+            post_id += 1
 
-    post = {'id': id, 'key': key, 'timestamp': timestamp, 'msg': msg, 'user_id': user_id, 'reply_to': reply_to}
-    posts.append(post)
+        key = generate_key()
+        timestamp = datetime.utcnow().isoformat()
 
-    return jsonify({'id': id, 'key': key, 'timestamp': timestamp, 'user_id': user_id, 'reply_to': reply_to}), 201
+        post = {'id': post_id, 'key': key, 'timestamp': timestamp, 'msg': message, 'user_id': user_id,
+                'parent_id': parent_id}
+        posts.append(post)
 
+        return jsonify({'id': post_id, 'key': key, 'timestamp': timestamp}), 201
 
-@app.route('/post/<int:id>', methods=['GET'])
-def read_post(id):
-    """Retrieve the post with the given ID."""
-    post = get_post_by_id(id)
-    if not post:
-        return jsonify({'err': 'Post not found'}), 404
+    @app.route('/post/<int:post_id>', methods=['GET'])
+    def get_post(post_id):
+        post = find_post_by_id(post_id)
+        if not post:
+            return jsonify({'err': 'Post not found'}), 404
 
-    user_id = post.get('user_id')
-    user = get_user_by_id(user_id) if user_id else None
-    user_unique_key = user['unique_key'] if user else None
+        children = [child_post['id'] for child_post in posts if child_post.get('parent_id') == post_id]
 
-    return jsonify(
-        {'id': post['id'], 'timestamp': post['timestamp'], 'msg': post['msg'], 'user_unique_key': user_unique_key,
-         'reply_to': post['reply_to']}), 200
+        return jsonify(
+            {'id': post['id'], 'timestamp': post['timestamp'], 'msg': post['msg'], 'parent_id': post.get('parent_id'),
+             'children': children}), 200
 
+    @app.route('/post/<int:post_id>/delete/<string:key>', methods=['DELETE'])
+    def delete_post(post_id, key):
+        post = find_post_by_id(post_id)
+        if not post:
+            return jsonify({'err': 'Post not found'}), 404
 
-@app.route('/post/<int:id>/delete', methods=['POST'])
-def delete_post(id):
-    """Delete the post with the given ID."""
-    post = get_post_by_id(id)
-    if not post:
-        return jsonify({'err': 'Post not found'}), 404
+        if post['key'] != key:
+            user = find_user_by_id(post.get('user_id'))
+            if not user or user['key'] != key:
+                return jsonify({'err': 'Forbidden'}), 403
 
-    if not request.is_json:
-        return jsonify({'err': 'Request body must be JSON'}), 400
+        posts.remove(post)
 
-    key = request.json.get('key')
-    if post['key'] != key:
-        user_id = request.json.get('user_id')
-        user_key = request.json.get('user_key')
-        user = get_user_by_id(user_id) if user_id else None
-        if not user or user['key'] != user_key or user['id'] != post['user_id']:
-            return jsonify({'err': 'Forbidden'}), 403
+        return jsonify({'id': post['id'], 'key': post['key'], 'timestamp': post['timestamp']}), 200
 
-    posts.remove(post)
+    @app.route('/posts', methods=['GET'])
+    def get_posts_in_range():
+        start_date_str = request.args.get('start')
+        end_date_str = request.args.get('end')
 
-    return jsonify({'id': post['id'], 'key': post['key'], 'timestamp': post['timestamp']}), 200
+        if start_date_str:
+            start_date = datetime.fromisoformat(start_date_str)
+        else:
+            start_date = None
 
+        if end_date_str:
+            end_date = datetime.fromisoformat(end_date_str)
+        else:
+            end_date = None
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        filtered_posts = []
+        for post in posts:
+            post_date = datetime.fromisoformat(post['timestamp'])
+            if (not start_date or post_date >= start_date) and (not end_date or post_date <= end_date):
+                filtered_posts.append(post)
+
+        return jsonify(filtered_posts), 200
+
+    @app.route('/user/<int:user_id>/posts', methods=['GET'])
+    def get_posts_by_user(user_id):
+        if not find_user_by_id(user_id):
+            return jsonify({'err': 'User not found'}), 404
+
+        filtered_posts = [post for post in posts if post.get('user_id') == user_id]
+
+        return jsonify(filtered_posts), 200
+
+    if __name__ == '__main__':
+        app.run(debug=True)
 
